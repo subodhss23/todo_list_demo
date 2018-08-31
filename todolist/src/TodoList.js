b5131a4082d235294d5ac9134c2d5496c5ee1856
Subodh Sharma <subodhss2323@gmail.com>
2018-08-30 23:17:59 -0700

    this is happening

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,19 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+ 
+class TodoList extends Component {
+    constructor(props){ 
+        super(props);
 
-class TodoList extends Component{
-    render(){
-        return(
-            <div className="todoListMain">
-                <div clasName="header">
-                    <form>
-                        <input placeholder="enter task">
-                        </input>
-                        <button type="submit">add</button>
-                    </form>
-                </div>
-            </div>
-        )
+        this.state = {
+            items: []
+        };
+
+        this.addItem = this.addItem.bind(this);
+    }
+
+    addItem(e){
+
     }
 }
 
+  render() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input ref={(a) => this._inputElement = a} placeholder="enter task">
+            </input>
+            <button type="submit">add</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+ 
 export default TodoList;
